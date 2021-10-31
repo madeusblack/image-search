25bda5f9918c5800113c670a9e9935fc3cda53ff
Yayson Olivares <kazeyaz@gmail.com>
2021-10-31 18:14:33 -0300

    added alt element to img files

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const App = () => {
         <div className='center'>
           {photos.map((photo) => (
             <article key={photo.id} onClick={() => open(photo.links.html)}>
-              <img src={photo.urls.regular}/>
+              <img alt ={photo.alt_description} src={photo.urls.regular}/>
               <p>{[photo.description,photo.alt_description].join(' - ')}</p>
             </article>
           ))}
